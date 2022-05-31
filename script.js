94d7364fa0b802f5b78ddc01589fb2aacf8bd8af
Anish <anishyadav7853@gmail.com>
2022-05-31 23:21:32 +0530

    added some new songs and logic for updating cover img and song name

--- script.js
+++ script.js
@@ -2,32 +2,34 @@ console.log("Welcome to Música");
 let songIndex = 0;
 let audioElement = new Audio('songs/Yaad Aati Nahin  Pawan Singh.mp3');
 let masterPlay = document.getElementById('masterPlay');
-let myProgressBar = document.getElementById('myProgressBar')
+let myProgressBar = document.getElementById('myProgressBar');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
+
 let songs = [{
         songName: "Yaad Aati Nahin - Pawan Singh",
         filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
         coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
     },
     {
-        songName: "Yaad Aati Nahin - Pawan Singh",
-        filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
-        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
+        songName: "Aa jaiha Pach ke - Pawan Singh",
+        filePath: "songs/Aa jaiha Pach ke - Pawan Singh.mp3",
+        coverPath: "cover/Aa Jaiha 5 ke pawan singh.jpg"
     }, {
-        songName: "Yaad Aati Nahin - Pawan Singh",
-        filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
-        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
+        songName: "Baarish Ban Jaana ( Bhojpuri ) - Pawan Singh",
+        filePath: "songs/Baarish Ban Jaana ( Bhojpuri ) - Pawan Singh.mp3",
+        coverPath: "cover/Baarish Ban jaana Pawan singh.jpg"
     }, {
-        songName: "Yaad Aati Nahin - Pawan Singh",
-        filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
-        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
+        songName: "Saari Se Tadi - Pawan Singh",
+        filePath: "songs/Saari Se Tadi - Pawan Singh.mp3",
+        coverPath: "cover/Saari se tadi Pawan singh.jpg"
     }, {
-        songName: "Yaad Aati Nahin - Pawan Singh",
-        filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
-        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
+        songName: "Tumsa Koi Pyaara - PAWAN SINGH & PRIYANKA SINGH  ",
+        filePath: "songs/Tumsa Koi Pyaara - PAWAN SINGH & PRIYANKA SINGH  .mp3",
+        coverPath: "cover/Tumsa Koi Pyaara Pawan singh.jpg"
     }, {
-        songName: "Yaad Aati Nahin - Pawan Singh",
-        filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
-        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
+        songName: "Zindagi 2 Mulaqaat Pawan Singh",
+        filePath: "songs/Zindagi 2 Mulaqaat Pawan Singh.mp3",
+        coverPath: "cover/Zindagi 2 Mulaqaat Pawan Singh.jpg"
     }, {
         songName: "Yaad Aati Nahin - Pawan Singh",
         filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
@@ -47,8 +49,13 @@ let songs = [{
     },
 ]
 
+songItems.forEach((element, i) => {
+    element.getElementsByTagName("img")[0].src = songs[i].coverPath;
+    element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+});
 // audioElement.play();//listen to events
 //handle play/pause click
+
 masterPlay.addEventListener('click', () => {
     if (audioElement.paused || audioElement.currentTime <= 0) {
         audioElement.play();
@@ -69,3 +76,9 @@ audioElement.addEventListener('timeupdate', () => {
 myProgressBar.addEventListener('change', () => {
     audioElement.currentTime = (myProgressBar.value * audioElement.duration) / 100;
 })
+
+
+function timestamp() {
+    document.getElementById('time').innerText = parseInt(audioElement.currentTime);
+}
+setInterval(timestamp, 1);
