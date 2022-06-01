c8e11328665281b5f405ac93b02089255fe601d3
Anish <anishyadav7853@gmail.com>
2022-06-01 15:44:23 +0530

    added some songs and added logic for navigation buttons

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 console.log("Welcome to Música");
 let songIndex = 0;
-let audioElement = new Audio('songs/Yaad Aati Nahin  Pawan Singh.mp3');
+let audioElement = new Audio('songs/1.mp3');
 let masterPlay = document.getElementById('masterPlay');
 let myProgressBar = document.getElementById('myProgressBar');
 let songItems = Array.from(document.getElementsByClassName('songItem'));
@@ -31,21 +31,21 @@ let songs = [{
         filePath: "songs/Zindagi 2 Mulaqaat Pawan Singh.mp3",
         coverPath: "cover/Zindagi 2 Mulaqaat Pawan Singh.jpg"
     }, {
-        songName: "Yaad Aati Nahin - Pawan Singh",
-        filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
-        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
+        songName: "Zindagi - Pawan Singh",
+        filePath: "songs/7.mp3",
+        coverPath: "cover/Zindagi Pawan Singh.jpg"
     }, {
-        songName: "Yaad Aati Nahin - Pawan Singh",
-        filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
-        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
+        songName: "Najariya Na Lage - Pawan Singh",
+        filePath: "songs/8.mp3",
+        coverPath: "cover/najariya na laage.jpg"
     }, {
-        songName: "Yaad Aati Nahin - Pawan Singh",
-        filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
-        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
+        songName: "Mohobat Ab Bechata  - Pawan Singh",
+        filePath: "songs/9.mp3",
+        coverPath: "cover/Mohobat ab bechata Pawan Singh.jpg"
     }, {
-        songName: "Yaad Aati Nahin - Pawan Singh",
-        filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
-        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
+        songName: "Lut gaye Bhojpuri - Pawan Singh",
+        filePath: "songs/10.mp3",
+        coverPath: "cover/Lut gaye Bhojpuri Pawan Singh.jpg"
     },
 ]
 
@@ -82,3 +82,57 @@ function timestamp() {
     document.getElementById('time').innerText = parseInt(audioElement.currentTime);
 }
 setInterval(timestamp, 1);
+
+const makeAllPlays = () => {
+    Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+        element.classList.add('fa-circle-play');
+        element.classList.remove('fa-circle-pause');
+
+    })
+}
+Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
+    element.addEventListener('click', (e) => {
+
+        makeAllPlays();
+        songIndex = parseInt(e.target.id)
+        e.target.classList.remove('fa-circle-play');
+        e.target.classList.add('fa-circle-pause');
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        document.getElementsByClassName('songInfo')[0].innerText = songs[songIndex].songName;
+        document.getElementsByClassName('cover')[0].src = songs[songIndex].coverPath;
+
+    })
+})
+
+document.getElementById('next').addEventListener('click', () => {
+    if (songIndex >= 9) {
+        songIndex = 0
+    } else {
+        songIndex += 1;
+    }
+    makeAllPlays();
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+    document.getElementsByClassName('songInfo')[0].innerText = songs[songIndex].songName;
+
+})
+document.getElementById('previous').addEventListener('click', () => {
+    if (songIndex <= 0) {
+        songIndex = 9;
+    } else {
+        songIndex -= 1;
+    }
+    audioElement.src = `songs/${songIndex+1}.mp3`;
+    audioElement.currentTime = 0;
+    audioElement.play();
+    masterPlay.classList.remove('fa-circle-play');
+    masterPlay.classList.add('fa-circle-pause');
+    document.getElementsByClassName('songInfo')[0].innerText = songs[songIndex].songName;
+})
