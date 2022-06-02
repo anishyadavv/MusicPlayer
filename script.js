2ec8e394b9c3cfcb55100bf04d8b4aba02cda53a
Anish <anishyadav7853@gmail.com>
2022-06-02 10:48:24 +0530

    added favicon and timestamps for every song

--- script.js
+++ script.js
@@ -8,50 +8,63 @@ let songItems = Array.from(document.getElementsByClassName('songItem'));
 let songs = [{
         songName: "Yaad Aati Nahin - Pawan Singh",
         filePath: "songs/Yaad Aati Nahin  Pawan Singh.mp3",
-        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg"
+        coverPath: "cover/Yaad Aati Nahin  Pawan Singh.jpg",
+        songDuration: "03:34"
     },
     {
         songName: "Aa jaiha Pach ke - Pawan Singh",
         filePath: "songs/Aa jaiha Pach ke - Pawan Singh.mp3",
-        coverPath: "cover/Aa Jaiha 5 ke pawan singh.jpg"
+        coverPath: "cover/Aa Jaiha 5 ke pawan singh.jpg",
+        songDuration: "04:15"
     }, {
         songName: "Baarish Ban Jaana ( Bhojpuri ) - Pawan Singh",
         filePath: "songs/Baarish Ban Jaana ( Bhojpuri ) - Pawan Singh.mp3",
-        coverPath: "cover/Baarish Ban jaana Pawan singh.jpg"
+        coverPath: "cover/Baarish Ban jaana Pawan singh.jpg",
+        songDuration: "03:24"
     }, {
         songName: "Saari Se Tadi - Pawan Singh",
         filePath: "songs/Saari Se Tadi - Pawan Singh.mp3",
-        coverPath: "cover/Saari se tadi Pawan singh.jpg"
+        coverPath: "cover/Saari se tadi Pawan singh.jpg",
+        songDuration: "03:20"
     }, {
         songName: "Tumsa Koi Pyaara - PAWAN SINGH & PRIYANKA SINGH  ",
         filePath: "songs/Tumsa Koi Pyaara - PAWAN SINGH & PRIYANKA SINGH  .mp3",
-        coverPath: "cover/Tumsa Koi Pyaara Pawan singh.jpg"
+        coverPath: "cover/Tumsa Koi Pyaara Pawan singh.jpg",
+        songDuration: "03:29"
     }, {
         songName: "Zindagi 2 Mulaqaat Pawan Singh",
         filePath: "songs/Zindagi 2 Mulaqaat Pawan Singh.mp3",
-        coverPath: "cover/Zindagi 2 Mulaqaat Pawan Singh.jpg"
+        coverPath: "cover/Zindagi 2 Mulaqaat Pawan Singh.jpg",
+        songDuration: "03:44"
     }, {
         songName: "Zindagi - Pawan Singh",
         filePath: "songs/7.mp3",
-        coverPath: "cover/Zindagi Pawan Singh.jpg"
+        coverPath: "cover/Zindagi Pawan Singh.jpg",
+        songDuration: "03:59"
     }, {
         songName: "Najariya Na Lage - Pawan Singh",
         filePath: "songs/8.mp3",
-        coverPath: "cover/najariya na laage.jpg"
+        coverPath: "cover/najariya na laage.jpg",
+        songDuration: "03:22"
     }, {
         songName: "Mohobat Ab Bechata  - Pawan Singh",
         filePath: "songs/9.mp3",
-        coverPath: "cover/Mohobat ab bechata Pawan Singh.jpg"
+        coverPath: "cover/Mohobat ab bechata Pawan Singh.jpg",
+        songDuration: "03:14"
     }, {
         songName: "Lut gaye Bhojpuri - Pawan Singh",
         filePath: "songs/10.mp3",
-        coverPath: "cover/Lut gaye Bhojpuri Pawan Singh.jpg"
+        coverPath: "cover/Lut gaye Bhojpuri Pawan Singh.jpg",
+        songDuration: "03:39"
     },
 ]
 
 songItems.forEach((element, i) => {
+
+
     element.getElementsByTagName("img")[0].src = songs[i].coverPath;
     element.getElementsByClassName("songName")[0].innerText = songs[i].songName;
+    element.getElementsByClassName("timestamp")[0].innerHTML = songs[i].songDuration;
 });
 // audioElement.play();//listen to events
 //handle play/pause click
@@ -73,10 +86,47 @@ audioElement.addEventListener('timeupdate', () => {
     //update seekbar
     progress = parseInt((audioElement.currentTime / audioElement.duration) * 100);
     myProgressBar.value = progress;
+    if (myProgressBar.value == 100) {
+        if (songIndex >= 9) {
+            songIndex = 0
+        } else {
+            songIndex += 1;
+        }
+        makeAllPlays();
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        document.getElementById('songInfoBottom').innerText = songs[songIndex].songName;
+        document.getElementById("cover").src = songs[songIndex].coverPath;
+        document.getElementById("cover1").src = songs[songIndex].coverPath;
+        document.getElementById("coverImg").src = songs[songIndex].coverPath;
+    }
 });
 myProgressBar.addEventListener('change', () => {
     audioElement.currentTime = (myProgressBar.value * audioElement.duration) / 100;
+    if (myProgressBar.value == 100) {
+        if (songIndex >= 9) {
+            songIndex = 0;
+        } else {
+            songIndex += 1;
+        }
+        makeAllPlays();
+        audioElement.src = `songs/${songIndex+1}.mp3`;
+        audioElement.currentTime = 0;
+        audioElement.play();
+        masterPlay.classList.remove('fa-circle-play');
+        masterPlay.classList.add('fa-circle-pause');
+        document.getElementById('songInfoBottom').innerText = songs[songIndex].songName;
+        document.getElementById("cover").src = songs[songIndex].coverPath;
+        document.getElementById("cover1").src = songs[songIndex].coverPath;
+        document.getElementById("coverImg").src = songs[songIndex].coverPath;
+    }
 })
+
+
+
 
 
 var minutes = 0;
@@ -92,7 +142,7 @@ function timestamp() {
         document.getElementById('time').innerText = "0" + minutes + ":" + seconds;
     }
 }
-setInterval(timestamp, 1);
+setInterval(timestamp, 1000);
 
 const makeAllPlays = () => {
     Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
@@ -101,6 +151,7 @@ const makeAllPlays = () => {
 
     })
 }
+
 Array.from(document.getElementsByClassName('songItemPlay')).forEach((element) => {
     element.addEventListener('click', (e) => {
 
